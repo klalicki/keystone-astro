efaff4e04b127dd46bd3dfab23adfa816e7df189
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-03-25 14:14:19 -0400

    trying to implement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  // WARNING: this is only needed for our monorepo examples, dont do this
+  vite: {
+    ssr: {
+      external: [
+        "@keystone-6/core/context",
+        "@keystone-6/core",
+        "@keystone-6/core/fields",
+      ],
+    },
+  },
+});
--- keystone.ts
+++ keystone.ts
@@ -5,25 +5,24 @@
 // Keystone imports the default export of this file, expecting a Keystone configuration object
 //   you can find out more at https://keystonejs.com/docs/apis/config
 
-import { config } from '@keystone-6/core';
+import { config } from "@keystone-6/core";
+import { lists } from "./src/keystone/schema";
 
-// to keep this file tidy, we define our schema in a different file
-import { lists } from './schema';
+export default config({
+  db: {
+    // we're using sqlite for the fastest startup experience
+    //   for more information on what database might be appropriate for you
+    //   see https://keystonejs.com/docs/guides/choosing-a-database#title
+    provider: "sqlite",
+    url: "file:./keystone.db",
 
-// authentication is configured separately here too, but you might move this elsewhere
-// when you write your list-level access control functions, as they typically rely on session data
-import { withAuth, session } from './auth';
-
-export default withAuth(
-  config({
-    db: {
-      // we're using sqlite for the fastest startup experience
-      //   for more information on what database might be appropriate for you
-      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
-    },
-    lists,
-    session,
-  })
-);
+    // WARNING: this is only needed for our monorepo examples, dont do this
+    //   we use using myprisma, not .myprisma, because vite
+    prismaClientPath: "node_modules/myprisma/client",
+  },
+  server: {
+    // We're using a custom port for this example so Astro and Keystone can run at the same time
+    port: 4000,
+  },
+  lists,
+});
